5a80e1ea7cdadd2a6be352a71b3e0f73ba2b9531
roemhildtg <roemhildtg@gmail.com>
2017-10-10 08:55:12 -0500

    fixes to make sure popupTemplate gets assigned

--- components/identify/identify.js
+++ components/identify/identify.js
@@ -16,6 +16,13 @@ const methods = {
     'esri.layers.MapImageLayer': mapImage
 };
 
+
+// asynchronously load the geometry engine
+let geometryEngine;
+esriPromise(['esri/geometry/geometryEngine']).then(([engine]) => {
+    geometryEngine = engine;
+});
+
 /**
  * identify class
  * performs identify on additional layers, like map image
@@ -44,7 +51,15 @@ export default DefineMap.extend({
     }, 
     identify (event) {
         this.view.popup.features = [];
-        const promises = [];
+
+        // map existing popup promises to es6 promises
+        const promises = this.view.popup.promises.map((p) => {
+            return new Promise((resolve) => {
+                p.then(resolve);
+            });
+        });
+
+        // for each layer, idenitify it, and push a promise
         this.view.map.layers.forEach((layer) => {
             if (!layer.visible) {
                 return;
@@ -52,59 +67,69 @@ export default DefineMap.extend({
             if (methods.hasOwnProperty(layer.declaredClass)) {
 
                 // get a promise that should resolve to some identify result
-                const promise = methods[layer.declaredClass](event, this.view, layer, this.layerInfos);
+                const _promise = methods[layer.declaredClass](event, this.view, layer, this.layerInfos);
+
+                const promise = new Promise((resolve) => {
+                    _promise.then((result) => {
+                        resolve({
+                            result: result,
+                            layerId: layer.id
+                        });
+                    });
+                });
                 promises.push(promise);
-                promise.then(this.identifyResolved.bind(this, layer.id, event));
             } else {
                 dev.warn(`no identify function registered for type ${layer.declaredClass}`);
             }
         });
-        Promise.all(promises).then(() => {
-            this.view.popup.open({
-                selectedFeatureIndex: 0
-            });
-        });
-    },
-    identifyResolved (layerId, event, result) {
-        if (result.results) { 
 
-            esriPromise(['esri/geometry/geometryEngine']).then(([GeometryEngine]) => {
+        // after all promises resolve, update the popup
+        Promise.all(promises).then((data) => {
 
-                
-                const features = result.results.map((props) => {
-
-                // get a default popup template from popupTemplates.layerId.sublayerId
-                    const template = get(this, `popupTemplates.${layerId}.${props.layerId}`) || {};
-
-                    // mixin props
-                    props.feature.popupTemplate = assign({
-                        title: props.layerName,
-                        content: defaultContent
-                    }, template.serialize ? template.serialize() : template);
-
-                    // return the modified feature
-                    return props.feature;
-                }).sort((a, b) => {
+            // reduce and sort to a plain array of features
+            const identifiedFeatures = data.reduce((a, b) => { 
+                return a.concat(this.assignPopupTemplate(b)); 
+            }, [])
+            
+            // sort according to distance from map click
+                .sort((a, b) => {
                     const geoms = [a, b].map((f) => {
                         return f.geometry.extent ? f.geometry.extent.center : f.geometry;
                     });
                     const distances = geoms.map((geom, index) => {
-                        return GeometryEngine.distance(event.mapPoint, geoms[index], 'feet');
+                        return geometryEngine.distance(event.mapPoint, geoms[index], 'feet');
                     });
                     const ret = distances[0] < distances[1] ? -1 : distances[0] > distances[1] ? 1 : 0;
                     return ret;
                 });
 
-                // add graphics ? doesn't work
-                // getGraphics(features).then((updatedFeatures) => {
-                // });
-                
-                // open the features
-                if (features.length) { 
-                    this.view.popup.features = this.view.popup.features.concat(features);
-                }
+            // concat with graphics already in the popup
+            const features = this.view.popup.features.concat(identifiedFeatures);
+
+            // open the popup with the given features
+            this.view.popup.open({
+                selectedFeatureIndex: 0,
+                features: features,
+                updateLocationEnabled: true
             });
-        }
+        });
+    },
+    assignPopupTemplate (data) {
+        const {layerId, result} = data;
+        return result.results ? result.results.map((props) => {
+
+            // get a default popup template from popupTemplates.layerId.sublayerId
+            const template = get(this, `popupTemplates.${layerId}.${props.layerId}`) || {};
+
+            // mixin props
+            props.feature.popupTemplate = assign({
+                title: props.layerName,
+                content: defaultContent
+            }, template.serialize ? template.serialize() : template);
+
+            // return the modified feature
+            return props.feature;
+        }) : [];
         
     }
 });
