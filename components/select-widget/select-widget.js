95d1995a6459126d268dd7c4d08698ad49347574
roemhildtg <roemhildtg@gmail.com>
2017-10-23 09:09:16 -0500

    remove uneeded missing import

--- components/select-widget/select-widget.js
+++ components/select-widget/select-widget.js
@@ -1,7 +1,6 @@
 import Component from 'can-component';
 import template from './template.stache';
 import ViewModel from './ViewModel';
-import './draw/polygon';
 
 import 'can-admin/components/form-widget/form-widget';
 import 'can-admin/components/nav-container/nav-container';
