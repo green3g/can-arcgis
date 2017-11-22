986dfc340022e9f08049d14b5b20b79b5383d2bd
roemhildtg <roemhildtg@gmail.com>
2017-11-22 11:41:47 -0600

    add layer.type as simplified alternative to specifying path

--- components/_common/createLayers.js
+++ components/_common/createLayers.js
@@ -1,7 +1,29 @@
 import defaultContent from './defaultContent';
 import esriPromise from 'esri-promise';
 import deepAssign from 'can-util/js/deep-assign/deep-assign';
-import assign from 'can-assign';
+
+const TYPES = {
+    csv: 'esri/layers/CSVLayer',
+    elevation: 'esri/layers/ElevationLayer',
+    feature: 'esri/layers/FeatureLayer',
+    rss: 'esri/layers/GeoRSSLayer',
+    graphics: 'esri/layers/GraphicsLayer',
+    group: 'esri/layers/Group',
+    imagery: 'esri/layers/ImageryLayer',
+    mesh: 'esri/layers/IntegratedMeshLayer',
+    kml: 'esri/layers/KMLLayer',
+    dynamic: 'esri/layers/MapImageLayer',
+    notes: 'esri/layers/MapNotesLayer',
+    osm: 'esri/layers/OpenStreetMapLayer',
+    pointCloud: 'esri/layers/PointCloudLayer',
+    scene: 'esri/layers/SceneLayer',
+    stream: 'esri/layers/StreamLayer',
+    tile: 'esri/layers/TileLayer',
+    vectorTile: 'esri/layers/VectorTileLayer',
+    webTile: 'esri/layers/WebTileLayer',
+    wms: 'esri/layers/WMSLayer',
+    wmts: 'esri/layers/WMTSLayer'
+};
 
 const Defaults = {
     outFields: ['*'],
@@ -16,13 +38,16 @@ const promise = new Promise((resolve) => {
 
 export default function createLayers (layers) {
     const layerPromises = layers.reverse().map((layer) => {
+
+        const path = layer.path ? layer.path : TYPES[layer.type] || 'esri/layers/UnknownLayer';
+
         return new Promise((resolve) => {
-            esriPromise([layer.path]).then(([LayerClass]) => {
+            esriPromise([path]).then(([LayerClass]) => {
 
                 const layerOptions = deepAssign({}, layer.options, Defaults);
 
                 // handle group layers
-                if (layer.path === 'esri/layers/GroupLayer') {
+                if (path === 'esri/layers/GroupLayer') {
                     const groupPromise = createLayers(layerOptions.layers || []);
                     groupPromise.then((newLayers) => {
                         layerOptions.layers = newLayers.map((l) => {
