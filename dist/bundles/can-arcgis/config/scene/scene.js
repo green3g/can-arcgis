91fa8c7a6500f4c12738cb7362c633bfdd7c51f6
roemhildtg <roemhildtg@gmail.com>
2017-12-12 22:52:58 -0600

    build

--- dist/bundles/can-arcgis/config/scene/scene.js
+++ dist/bundles/can-arcgis/config/scene/scene.js
@@ -1,16 +1,2 @@
-/*can-arcgis@1.0.0#config/scene/scene*/
-define('can-arcgis@1.0.0#config/scene/scene', ['exports'], function (exports) {
-    'use strict';
-    Object.defineProperty(exports, '__esModule', { value: true });
-    exports.default = {
-        debug: true,
-        mapOptions: {
-            type: 'WebScene',
-            portalItem: { id: 'b1f8fb3b2fd14cc2a78728de108776b0' }
-        },
-        viewOptions: {
-            type: 'SceneView',
-            environment: { lighting: { directShadowsEnabled: false } }
-        }
-    };
-});
+define("can-arcgis@1.0.0#config/scene/scene",["exports"],function(e){"use strict";Object.defineProperty(e,"__esModule",{value:!0}),e.default={debug:!0,mapOptions:{type:"WebScene",portalItem:{id:"b1f8fb3b2fd14cc2a78728de108776b0"}},viewOptions:{type:"SceneView",environment:{lighting:{directShadowsEnabled:!1}}}}});
+//# sourceMappingURL=scene.js.map
