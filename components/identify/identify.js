3d9d000bdd3c64a916b26120c3410d66fc7b0933
roemhildtg <roemhildtg@gmail.com>
2017-12-26 11:06:04 -0600

    identify sorting enhancements - feature layers should show up on top

--- components/identify/identify.js
+++ components/identify/identify.js
@@ -1,7 +1,12 @@
 import DefineMap from 'can-define/map/map';
 import mapImage from './util/identifyMapImage';
 import dev from 'can-util/js/dev/dev';
-import esriPromise from 'esri-promise';
+import esriPromise from 'esri-promise'; 
+// asynchronously load the geometry engine
+let geometryEngine;
+esriPromise(['esri/geometry/geometryEngine']).then((modules) => {
+    [geometryEngine] = modules;
+});
 
 export const IDENTIFY_METHODS = {
     'esri.layers.MapImageLayer': mapImage,
@@ -20,12 +25,6 @@ export const IDENTIFY_METHODS = {
     }
 };
 
-
-// asynchronously load the geometry engine
-let geometryEngine;
-esriPromise(['esri/geometry/geometryEngine']).then((modules) => {
-    [geometryEngine] = modules;
-});
 
 /**
  * identify class
@@ -60,22 +59,53 @@ export default DefineMap.extend({
         this.view.popup.features = [];
 
         const promises = [];
+        const layers = this.view.map.allLayers;
+
+        // put feature layers on top
+        // don't sort the allLayers array, get a copy
+        layers.toArray().sort((a, b) => {
+            if (a.declaredClass === 'esri.layers.FeatureLayer') {
+                return -1;
+            }
+            const aIndex = layers.indexOf(a);
+            const bIndex = layers.indexOf(b);
+            return bIndex - aIndex;
+        })
 
         // for each layer, idenitify it, and push a promise
-        this.view.map.allLayers.forEach((layer) => {
-            if (!layer.visible) {
-                return;
-            }
-            if (IDENTIFY_METHODS.hasOwnProperty(layer.declaredClass)) {
+            .forEach((layer) => {
+
+                if (!layer.visible) {
+                    return;
+                }
+                if (IDENTIFY_METHODS.hasOwnProperty(layer.declaredClass)) {
 
                 // get a promise that should resolve to some identify result
-                const promise = IDENTIFY_METHODS[layer.declaredClass](event, layer, this);
+                    const promise = new Promise((resolve) => {
+                        let isResolved = false;
+                        IDENTIFY_METHODS[layer.declaredClass](event, layer, this).then((result) => {
+                            if (isResolved) {
+                                dev.warn('identify: promise was already resolved');
+                            }
+                            isResolved = true;
+                            resolve(result);
+                        });
 
-                promises.push(promise);
-            } else {
-                dev.warn(`no identify function registered for type ${layer.declaredClass}`);
-            }
-        });
+                        setTimeout(() => {
+                            if (isResolved) {
+                                return;
+                            }                            
+                            dev.warn('identify: promise timeout exceeded');
+                            resolve([]);
+                        }, 5000);
+                    });
+                
+
+                    promises.push(promise);
+                } else {
+                    dev.warn(`identify: no identify function registered for type ${layer.declaredClass}`);
+                }
+            });
 
         // after all promises resolve, update the popup
         const identifyPromise = new Promise((resolve) => {
@@ -84,19 +114,30 @@ export default DefineMap.extend({
             // reduce and sort to a plain array of features
                 const identifiedFeatures = data.reduce((a, b) => { 
                     return a.concat(b); 
-                }, [])
+                }, []);
             
                 // sort according to distance from map click
-                    .sort((a, b) => {
-                        const geoms = [a, b].map((f) => {
-                            return f.geometry.extent ? f.geometry.extent.center : f.geometry;
-                        });
-                        const distances = geoms.map((geom, index) => {
-                            return geometryEngine.distance(event.mapPoint, geoms[index], 'feet');
-                        });
-                        const ret = distances[0] < distances[1] ? -1 : distances[0] > distances[1] ? 1 : 0;
-                        return ret;
+                identifiedFeatures.sort((a, b) => {
+                    const geoms = [a, b].map((f) => {
+                        return f.geometry.extent ? f.geometry.extent.center : f.geometry;
                     });
+                    const distances = geoms.map((geom, index) => {
+                        return geometryEngine.distance(event.mapPoint, geoms[index], 'feet');
+                    });
+                        
+                    const ret = distances[0] - distances[1];//distances[0] < distances[1] ? -1 : distances[0] > distances[1] ? 1 : 0;
+                        
+                    // if distance is a tie, sort so feature layers come first
+                    if (Math.round(ret * 100) / 100 === 0) {
+                        if (a.layer && a.layer.declaredClass === 'esri.layers.FeatureLayer') {
+                            return -1;
+                        }
+                        if (b.layer && b.layer.declaredClass === 'esri.layers.FeatureLayer') {
+                            return 1;
+                        }
+                    }
+                    return ret;
+                });
 
                 if (identifiedFeatures.length) { 
                     this.view.popup.open({
