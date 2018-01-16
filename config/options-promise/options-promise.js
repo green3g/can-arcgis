72f0d7aaaadb2ca612e2e73a815645e80a576579
roemhildtg <roemhildtg@gmail.com>
2018-01-16 17:14:58 -0600

    get rid of extra promise

--- config/options-promise/options-promise.js
+++ config/options-promise/options-promise.js
@@ -17,26 +17,24 @@ export default {
         parent: 'view',
         position: 'top-right',
         path: 'esri/widgets/BasemapToggle',
-        optionsPromise: new Promise((resolve) => {
-            loadModules(['esri/Basemap', 'esri/layers/WebTileLayer'])
-                .then(([BaseMap, WebTileLayer]) => {
-                    resolve({
-                        titleVisible: true,
-                        nextBasemap: new BaseMap({
-                            title: 'Terrain',
-                            id: 'terrain',
-                            thumbnailUrl,
-                            baseLayers: [new WebTileLayer({
-                                urlTemplate: 'http://{subDomain}.tile.stamen.com/terrain/{level}/{col}/{row}.png',
-                                subDomains: ['a', 'b', 'c', 'd'],
-                                copyright: 'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, ' +
+        optionsPromise: loadModules(['esri/Basemap', 'esri/layers/WebTileLayer'])
+            .then(([BaseMap, WebTileLayer]) => {
+                return {
+                    titleVisible: true,
+                    nextBasemap: new BaseMap({
+                        title: 'Terrain',
+                        id: 'terrain',
+                        thumbnailUrl,
+                        baseLayers: [new WebTileLayer({
+                            urlTemplate: 'http://{subDomain}.tile.stamen.com/terrain/{level}/{col}/{row}.png',
+                            subDomains: ['a', 'b', 'c', 'd'],
+                            copyright: 'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, ' +
                               'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ' +
                               'Data by <a href="http://openstreetmap.org/">OpenStreetMap</a>, ' +
                               'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
-                            })]
-                        })
-                    });
-                });
-        })
+                        })]
+                    })
+                };
+            })
     }]
 };
