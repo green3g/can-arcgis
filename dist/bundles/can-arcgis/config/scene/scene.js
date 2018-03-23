0cf09cf85755466af49008086a539f053fb5019b
roemhildtg <roemhildtg@gmail.com>
2018-03-23 09:49:59 -0500

    build

--- dist/bundles/can-arcgis/config/scene/scene.js
+++ dist/bundles/can-arcgis/config/scene/scene.js
@@ -1,2 +1,2 @@
-define("can-arcgis@2.2.2#config/scene/scene",["exports"],function(e){"use strict";Object.defineProperty(e,"__esModule",{value:!0}),e.default={title:"Loading a 3d map",debug:!0,mapOptions:{basemap:"satelite",type:"WebScene",portalItem:{id:"b1f8fb3b2fd14cc2a78728de108776b0"}},viewOptions:{type:"SceneView"}}});
+define("can-arcgis@2.2.5#config/scene/scene",["exports"],function(e){"use strict";Object.defineProperty(e,"__esModule",{value:!0}),e.default={title:"Loading a 3d map",debug:!0,mapOptions:{basemap:"satelite",type:"WebScene",portalItem:{id:"b1f8fb3b2fd14cc2a78728de108776b0"}},viewOptions:{type:"SceneView"}}});
 //# sourceMappingURL=scene.js.map
